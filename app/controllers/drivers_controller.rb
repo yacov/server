153c5ff1b62b9b9f4106e14c4bef353d15165677
yacov <jakoff@gmail.com>
2014-02-08 20:44:21 +0200

    Changes

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,6 @@
 class DriversController < ApplicationController
 
-  before_filter :filter_driver, only: [:order, :accept, :cancel, :point, :status]
+  before_filter :filter_driver, only: [:order, :accept, :cancel, :point, :status, :took]
 
   def share
     u=User.where(ref: params[:ref]).first
@@ -12,11 +12,11 @@ class DriversController < ApplicationController
 
   def register
     #error, if device_id not matches regular expression rule
-    return render json: {errors: "Bad device id"}, status: 400 if params[:device_id] !~ /./
+    return render json: {status:"ERROR", message: "Bad device id"}, status: 400 if params[:device_id] !~ /./
 
     # check whether there is a driver with the id or not
     dr=User.where(device_id: params[:device_id]).first
-    return render json: {errors: "As the driver, you have already taken place"} if dr and dr.driver
+    return render json: {status:"ERROR",message: "As the driver, you have already taken place"} if dr and dr.driver
 
 
     #this is line that makes you love ruby on rails, hope it's readable:
@@ -31,20 +31,20 @@ class DriversController < ApplicationController
                                 #d=u.create_driver(name: params[:name],car_id: params[:car_id], brand: params[:brand])
                                 #u.create_driver(name: params[:name],car_id: params[:car_id], brand: params[:brand])
 
-    return render json: {id: u.id, ref: u.ref }
+    return render json: {status:"OK",id: u.id, ref: u.ref }
   end
 
   def point
-    return render json: {id: @u.id, points: @u.points}
+    return render json: {status:"OK", id: @u.id, points: @u.points}
   end
 
   def order
-    return render json: {errors: "Yu nat driver"} if !d=@u.driver
-    gps_long_drivers = params[:gps_long_driver].to_f
-    gps_lat_drivers = params[:gps_lat_driver].to_f
+    return render json: {status:"ERROR", message: "Yu nat driver"} if !d=@u.driver
+    gps_long_drivers = params[:gps_long_drivers].to_f
+    gps_lat_drivers = params[:gps_lat_drivers].to_f
     result=Order.find_by_sql"SELECT *, ((ACOS(SIN(#{gps_lat_drivers} * PI() / 180) * SIN(`gps_lat_user` * PI() / 180) + COS(#{gps_lat_drivers} * PI() / 180) * COS(`gps_lat_user` * PI() / 180) * COS((#{gps_long_drivers} - `gps_long_user`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS distance FROM orders WHERE status = #{Order::Status::OPEN} HAVING distance <= 3 ORDER BY distance LIMIT 10"
-    return render :json => result.map{|result|{:id=> result.id, :user_id=> result.user_id, :dist=>result.distance.to_f, :address=> result.address }} if result                                                                                                                                                #* 6378245
-    return render json: {error:"no orders"}
+    return render :json => result.map{|result|{status:"OK", :id_result=> result.id, :user_id=> result.user_id, :dist=>result.distance.to_f, :address=> result.address }} if result!=nil                                                                                                                                                #* 6378245
+    return render json: {status:"ERROR", message:"no orders"}
 
     #result = Order.find_by_sql" SELECT *, 2 * 6371* ASIN(SQRT(POW(SIN(((#{gps_lat_drivers}-gps_lat_user) / 2)*PI()/180), 2)+COS(#{gps_lat_drivers}*PI()/180) * COS(gps_lat_user*PI()/180) *POW(SIN(((#{gps_long_drivers}- gps_long_user) / 2)*PI()/180), 2)))  AS distance FROM orders WHERE status = #{Status::OPEN} HAVING distance <= 5 ORDER BY distance LIMIT 10"
     #result = Order.find_by_sql" SELECT *, 2 * 3956* ASIN(SQRT(POW(SIN((#{gps_lat_drivers}-gps_lat_user)*PI()/180 / 2), 2)+COS(#{gps_lat_drivers}*PI()/180) * COS(gps_lat_user*PI()/180) *POW(SIN((#{gps_long_drivers}- gps_long_user)*PI()/180 / 2), 2)))  AS distance FROM orders WHERE status = #{Status::OPEN} HAVING distance <= 5 ORDER BY distance LIMIT 10"
@@ -52,46 +52,46 @@ class DriversController < ApplicationController
   end
 
   def status
-      return render json: {errors: "Order with this id does not exist"} if !o=@u.driver.orders.find_by_id(params[:order_id])
-      return render json: {errors: "Order this id is closed"} if o.status==Order::Status::CLOSED
-      return render json: {errors: "Order this id closed user "} if o.status==Order::Status::UsersClosed
-      return render json: {errors: "Order this you closed"} if o.status==Order::Status::DriversClosed
+      return render json: {status:"ERROR", message: "Order with this id does not exist"} if !o=@u.driver.orders.find_by_id(params[:order_id])
+      return render json: {status:"ERROR", message: "Order this id is closed"} if o.status==Order::Status::CLOSED
+      return render json: {status:"ERROR", message: "Order this id closed user "} if o.status==Order::Status::UsersClosed
+      return render json: {status:"ERROR", message: "Order this you closed"} if o.status==Order::Status::DriversClosed
       o.update_attributes(gps_long_drivers: params[:gps_long_drivers],gps_lat_drivers: params[:gps_lat_drivers])
-      return render json: {mesage: " 1 "}
+      return render json: {status:"OK"}
   end
 
   def accept
     #return render json: {error: "order is already taken"} if @u.driver.orders.where(status: Order::Status::SELECT).first
     o=Order.where(:id => params[:order_id]).first
-    return render json: {error: "Orders busy"} if (o.status==Order::Status::SELECT)      # -check- -> #  else return render json: {text: "order tut"}
+    return render json: {status:"ERROR", message: "Orders busy"} if (o.status==Order::Status::SELECT)      # -check- -> #  else return render json: {text: "order tut"}
     d=@u.driver
     d.orders<<o
     o.update_attributes(gps_long_drivers: params[:gps_long_drivers],gps_lat_drivers: params[:gps_lat_drivers], status: Order::Status::SELECT)
     @u.points=@u.points-1
-    return render json: {message:"Your order"}
+    return render json: {status:"OK", message:"Your order"}
   end
 
   def cancel
-    return render json: {errors: "Yu nat driver"} if !d=@u.driver
+    return render json: {status:"ERROR", message: "Yu nat driver"} if !d=@u.driver
     o=d.orders.where(id: params[:order_id],status: Order::Status::SELECT).first
     o.status=Order::Status::DriversClosed
     o.save
     @u.points=@u.points-1
-    u.save
-    return render json: {message: "Taken you order transferred to the free mode"}
+    @u.save
+    return render json: {status:"OK", message: "Taken you order transferred to the free mode"}
   end
 
   def took
     o=@u.driver.orders.find_by_id(params[:order_id])
-    o.status=Order::Status::CLOSED if o.status==Order::Status::DriversTook
-    o.status=Order::Status::UsersVillages if o.status== Order::Status::SELECT
-    return render json: {mesage: 'Thank you for using our application'}
+    o.update_attribute(:status, Order::Status::CLOSED) if o.status==Order::Status::UsersVillages
+    o.update_attribute(:status, Order::Status::DriversTook) if o.status== Order::Status::SELECT
+    return render json: {status:"OK", message: 'Thank you for using our application'}
   end
 
 
   private
   def filter_driver
-    return render json: {error: "This id does not exist in the database"} if !(@u=User.find_by_id(params[:driver_id]))
+    return render json: {status:"ERROR",message: "This id does not exist in the database"} if !(@u=User.find_by_id(params[:driver_id]))
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   end
   def register
     #error, if device_id not matches regular expression rule
-    return render json: {errors: "Bad device id"}, status: 400 if params[:device_id] !~ /./
-    return render json: {errors: ""} if User.find_by_device_id(params[:device_id])
+    return render json: {status:"ERROR",message: "Bad device id"}, status: 400 if params[:device_id] !~ /./
+    return render json: {status:"ERROR" } if u_temp=User.find_by_device_id(params[:device_id])
     
     #this is line that makes you love ruby on rails, hope it's readable:
     # find user with device_id
@@ -22,50 +22,52 @@ class UsersController < ApplicationController
     u.email = params[:email] if params[:email] =~ /.@.+\..+/ #TODO: stronger email validation
     u.phone = params[:phone]
     u.save                 #will create new or update existing
-    return render json: {id: u.id, ref: u.ref}
+    return render json: {status:"OK",id_users: u.id, ref: u.ref}
   end
   def order
-    return render json: {errors: "Yu driver"} if d=@u.driver            # check that the order is not the driver draws
-    return render json: {errors: "Your order is still open"} if @u.orders.find_by_status(Order::Status::OPEN) || @u.orders.find_by_status(Order::Status::SELECT)
+    return render json: {status:"ERROR",message: "Yu driver"} if d=@u.driver            # check that the order is not the driver draws
+    return render json: {status:"ERROR",message: "Your order is still open"} if @u.orders.find_by_status(Order::Status::OPEN) || @u.orders.find_by_status(Order::Status::SELECT)
     o=@u.orders.create(:address=>params[:address],:gps_long_user=>params[:gps_long_user],:gps_lat_user=>params[:gps_lat_user],:status=>Order::Status::OPEN)
     #@u.points=@u.points-1 if @u.points > 0
     @u.save
-    return render json: {id_o: o.id, id_u: @u.id}
+    return render json: {status:"OK",id_orders: o.id}
   end
   def status
-    return render json: {errors: "Order with this id does not exist"} if !o=@u.orders.find_by_id(params[:order_id])
-    return render json: {errors: "Order this id is not accepted"} if o.status==Order::Status::OPEN
-    return render json: {errors: "Order this id is closed"} if o.status==Order::Status::CLOSED
-    return render json: {errors: "Order this id closed driver "} if o.status==Order::Status::DriversClosed
-    return render json: {errors: "Order this you closed"} if o.status==Order::Status::UsersClosed
+    return render json: {status:"ERROR",message: "Order with this id does not exist"} if !o=@u.orders.find_by_id(params[:order_id])
+    return render json: {status:"ERROR",message: "Order this id is not accepted"} if o.status==Order::Status::OPEN
+    return render json: {status:"ERROR",message: "Order this id is closed"} if o.status==Order::Status::CLOSED
+    return render json: {status:"ERROR",message: "Order this id closed driver "} if o.status==Order::Status::DriversClosed
+    return render json: {status:"ERROR",message: "Order this you closed"} if o.status==Order::Status::UsersClosed
     d=o.driver
-    return render json: {name: d.name, gps_long_drivers: o.gps_long_drivers, gps_lat_drivers: o.gps_lat_drivers}
+    ud=o.driver.user
+    return render json: {status:"OK",name: d.name, gps_long_drivers: o.gps_long_drivers, gps_lat_drivers: o.gps_lat_drivers,brand_car:d.brand,car_id: d.car_id,phone:ud.phone  }
   end
   def cancel
-     return render json: {error: "It id not open order"} if !o=@u.orders
-     o.find_by_id(params[:order_id]).update_attribute(:status, Order::Status::UserCansel)
-     return render json: {text: "Order closed"}
+     return render json: {status:"ERROR",message: "It id not open order"} if !o=@u.orders
+     o.find_by_id(params[:order_id]).update_attribute(:status, Order::Status::UsersClosed)
+     return render json: {status:"OK",message: "Order closed"}
   end
   def point
-    return render json: {id: @u.id, points: @u.points}
+    return render json: {id_users: @u.id, points: @u.points}
   end
 
   def payment
-    return render json: {error: "your account is not enough money to pay for the order"} if @u.points.to_i<0 || @u.points.to_i<params[:points].to_i
+    return render json: {status:"ERROR",message: "your account is not enough money to pay for the order"} if @u.points.to_i<0 || @u.points.to_i<params[:points].to_i
     @u.points=@u.points.to_i - params[:points].to_i
-    return render json: {error: "error in order"} if !o=@u.orders.where(status: Order::Status::SELECT).first
+    return render json: {status:"ERROR",message: "error in order"} if !o=@u.orders.where(status: Order::Status::SELECT).first
     user_driver=User.find_by_id(o.driver_id)
     user_driver.points=user_driver.points.to_i+params[:points].to_i
     @u.save
     user_driver.save
-    return render json: {masage: "payment went"}
+    return render json: {status:"OK",message: "payment went"}
   end
 
   def withdrawn
+
     o=@u.orders.find_by_id(params[:order_id])
-    o.status=Order::Status::CLOSED if o.status==Order::Status::DriversTook
-    o.status=Order::Status::UsersVillages if o.status== Order::Status::SELECT
-    return render json: {mesage: 'Thank you for using our application'}
+    o.update_attribute(:status, Order::Status::CLOSED) if o.status==Order::Status::DriversTook
+    o.update_attribute(:status, Order::Status::UsersVillages)  if o.status== Order::Status::SELECT
+    return render json: {status:"OK",message: 'Thank you for using our application'}
   end
 
   def status_stat
@@ -78,7 +80,7 @@ class UsersController < ApplicationController
   end
   private
   def filter_user
-    return render json: {error: "This id does not exist in the database"} if !(@u=User.find_by_id(params[:user_id]))
+    return render json: {status:"ERROR",message: "This id does not exist in the database"} if !(@u=User.find_by_id(params[:user_id]))
   end
 
 end
